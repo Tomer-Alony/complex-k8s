1d335e1f7469516cab2dfeb00786dc2233078eab
Tomer Alony <sturs23@gmail.com>
2020-10-24 21:03:07 +0300

    fixing docker push, missing -t in commands

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 docker build -t sturs23/complex-k8s-client:latest -t sturs23/complex-k8s-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t sturs23/complex-k8s-server:latest -t sturs23/complex-k8s-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t sturs23/complex-k8s-worker:latest -t sturs23/complex-k8s-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-docker push sturs23/complex-k8s-worker:latest sturs23/complex-k8s-worker:$GIT_SHA
-docker push sturs23/complex-k8s-server:latest sturs23/complex-k8s-server:$GIT_SHA
-docker push sturs23/complex-k8s-client:latest sturs23/complex-k8s-client:$GIT_SHA
+docker push -t sturs23/complex-k8s-worker:latest sturs23/complex-k8s-worker:$GIT_SHA
+docker push -t sturs23/complex-k8s-server:latest sturs23/complex-k8s-server:$GIT_SHA
+docker push -t sturs23/complex-k8s-client:latest sturs23/complex-k8s-client:$GIT_SHA
 kubectl apply -f ./k8s
 
 kubectl set image deployments/server-deployment server=sturs23/complex-k8s-server:$GIT_SHA
