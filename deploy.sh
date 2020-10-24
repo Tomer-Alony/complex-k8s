fb14bc99b68539617d7dd4d83e82c2bda56abcda
Tomer Alony <sturs23@gmail.com>
2020-10-24 20:49:23 +0300

    added gcloud and travis config for prod

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+docker build -t sturs23/complex-k8s-client:latest -t sturs23/complex-k8s-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t sturs23/complex-k8s-server:latest -t sturs23/complex-k8s-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t sturs23/complex-k8s-worker:latest -t sturs23/complex-k8s-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push sturs23/complex-k8s-worker:latest sturs23/complex-k8s-worker:$GIT_SHA
+docker push sturs23/complex-k8s-server:latest sturs23/complex-k8s-server:$GIT_SHA
+docker push sturs23/complex-k8s-client:latest sturs23/complex-k8s-client:$GIT_SHA
+kubectl apply -f ./k8s
+
+kubectl set image deployments/server-deployment server=sturs23/complex-k8s-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=sturs23/complex-k8s-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=sturs23/complex-k8s-worker:$GIT_SHA
